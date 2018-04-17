0da048a1a54fd12184969a9bd3219c79a341f86d
MarkCangila <38412104+MarkCangila@users.noreply.github.com>
2018-04-17 14:28:41 -0400

    welcome.php possible

--- welcome.php
+++ welcome.php
@@ -1,5 +1,8 @@
-<?php
-$myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
-fwrite($myfile, $name);
-fwrite($myfile, $message);
-?>
+<html>
+  <body>
+    <p id='no'></p>
+    <script type="application/javascript">
+      document.getElementById('no').innerHTML += <?php echo $_POST["name"]; ?>: <?php echo $_POST["email"]; ?>;
+    </script>
+  </body>
+</html>
