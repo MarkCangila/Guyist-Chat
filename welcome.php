0be0b91f37fe10fc0fad2d054ee55153550a6c2e
MarkCangila <38412104+MarkCangila@users.noreply.github.com>
2018-04-17 15:10:44 -0400

    Delete old .php

--- welcome.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-
-<?php file_put_contents ( "./text.txt", "<?php echo $_GET["name"]; ?>: <?php echo $_GET["email"]; ?>", FILE_APPEND)?>
-header('Location: index.html');
-exit;
-
-</body>
-</html>
